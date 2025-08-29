cf8a7d1ee9320bcaa78addfe8b41fd141308e1a6
yanjiewu <yw4445@columbia.edu>
2025-08-29 22:54:44 +0800

    Initial Commit

---
+++ VoiceJournal/VoiceJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceJournalApp.swift
+//  VoiceJournal
+//
+//  Created by Wu Yanjie on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
